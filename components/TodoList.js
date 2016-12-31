3f8ce15506693e95b72ce00df7e41ea83cb1ba52
tusharkoul <tusharkoul@gmail.com>
2016-12-30 21:17:47 -0800

    extracting TodoItem so that list component can be extended

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
+import TodoItem from './TodoItem'
 
 class TodoList extends Component {
 
     createTodoList() {
         return this.props.todos.map((todoItem) => {
-            return <li key={todoItem.id}> {todoItem.text} </li>
+            //return <li key={todoItem.id}> {todoItem.text} </li>
+            return (<TodoItem key={todoItem.id} todo={todoItem}/>);   // key is needed any time we use a for loop
         });
     }
 
     render() {
-        console.log("rendering list with props",this.props)
+        console.log("rendering list with props",this.props);
         return (
             <ul>
                 {this.createTodoList()}
